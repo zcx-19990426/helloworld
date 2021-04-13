a7eebbc76998dc0ddca640234e470fb1c990acbf
zcx-19990426 <404506274@qq.com>
2021-04-13 08:20:41 +0800

    version2.0

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("这是我的第一个git上传项目！！！");
         System.out.println("这是我项目尊贵的第二个版本！！！");
+        System.out.println("这是一次实验");
     }
 }
